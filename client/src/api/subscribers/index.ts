f92d77c728b5c228041f2eea5f291a386ba66cf5
manihateu <102178609+manihateu@users.noreply.github.com>
2023-12-17 16:49:58 +0300

    subscriberApis

--- client/src/api/subscribers/index.ts
+++ client/src/api/subscribers/index.ts
@@ -1,3 +1,59 @@
-//apis
+import $host from "../interceptor"
+import { createSubscriberDto } from "./subscriber.dto"
 
-export default () => {}
+const createSubscriber = async(dto: createSubscriberDto) => {
+    try{
+        const res = await $host.post('/subscriber/create', dto)
+        return res.data
+    }
+    catch(e){
+        return 'Error - ' + e
+    }
+}
+
+const getSubscribersById = async(id: number) => {
+    try{
+        const res = await $host.get(`/subscriber/${id}`)
+        return res.data
+    }
+    catch(e){
+        return 'Error - ' + e
+    }
+}
+
+const getSubscribers = async() => {
+    try{
+        const res = await $host.get('/subscriber')
+        return res.data
+    }
+    catch(e){
+        return 'Error - ' + e
+    }
+}
+
+const updateSubscriber = async(id: number) => {
+    try{
+        const res = await $host.get(`/subscriber/update/${id}`)
+        return res.data
+    }
+    catch(e){
+        return 'Error - ' + e
+    }
+}
+
+const deleteSubscriber = async(id: number) => {
+    try{
+        const res = await $host.delete(`/subscriber/${id}`)
+        return res.data
+    }
+    catch(e){
+        return 'Error - ' + e
+    }
+}
+export default {
+    createSubscriber,
+    getSubscribers,
+    getSubscribersById,
+    updateSubscriber,
+    deleteSubscriber
+}
